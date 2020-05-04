80f71d344ae5ae999e97c7c2b80b7fa8c3395825
Justin Golden <justingolden21@gmail.com>
2020-05-04 03:54:06 -0700

    clean code

--- script.js
+++ script.js
@@ -17,14 +17,8 @@ $( ()=> {
 		finGrade.prop('disabled', !calcFinGrade);
 		assScore.prop('disabled', calcFinGrade);
 
-		if(!calcFinGrade) {
-			finGrade.parent().find('.input-group-text').addClass('highlight');
-			assScore.parent().find('.input-group-text').removeClass('highlight');
-		}
-		else {
-			assScore.parent().find('.input-group-text').addClass('highlight');
-			finGrade.parent().find('.input-group-text').removeClass('highlight');
-		}
+		finGrade.parent().find('.input-group-text').toggleClass('highlight');
+		assScore.parent().find('.input-group-text').toggleClass('highlight');
 	});
 
 	updateGrade();
